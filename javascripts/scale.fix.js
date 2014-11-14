bc8a17dff4c14c562d5fe99519867ad5d81c2412
Julyyq <colorfuljuly@gmail.com>
2014-11-14 13:35:24 +0800

    update isMobile

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,7 +1,7 @@
 var metas = document.getElementsByTagName('meta');
 var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
       metas[i].content = "minimum-scale=1.0, maximum-scale=1.0";
     }
